2ae4b19502c97f49fc26bd46c7232244e37a6b5d
r.koshkarov <r.koshkarov@MAC364RDS-2.local>
2016-07-21 14:40:49 +0300

    seelector child

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ class Ram {
 
   dispatch(e, el) {
     let target = e.target;
-    if (this.element) {
+    let element = ((typeof this.selector) != 'string') ? this.selector : this.s.querySelector(this.selector);
+
+    if (this.selector) {
       while (target !== this.s) {
-        if (target === this.element) {
-          if (typeof this.callback === 'function') this.callback(e, this.element);
+        if (target === element) {
+          if (typeof this.callback === 'function') this.callback(e, this.selector);
           break;
         }
         target = target.parentNode;
@@ -35,10 +37,10 @@ class Ram {
     }, false);
   }
 
-  prepare(type, event, callback, element) {
+  prepare(type, event, callback, selector) {
     this.event = event;
     this.callback = callback;
-    if (element) this.element = element;
+    if (selector) this.selector = selector;
     // Set listeners on all child items. Ex. for ul li
     if (this.scope.length > 1 && this.scope.tagName !== 'FORM') {
       for (let x = 0; x < this.scope.length; x++) {
@@ -51,8 +53,8 @@ class Ram {
     }
   }
 
-  on(event, callback, element) {
-    this.prepare('on', event, callback, element);
+  on(event, callback, selector) {
+    this.prepare('on', event, callback, selector);
   }
 
   off(event, callback) {
